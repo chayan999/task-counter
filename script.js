acf6bfe29270d1e789bfbee93da46ba18f2f4e55
Chayan Chandan das <chayanchandandas@gmail.com>
2021-09-13 21:22:02 +0600

    add js function

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 
 const counter = document.querySelectorAll('.count');
 
-counter.forEach((countr) => {
-    console.log(countr)
+counter.forEach((counter) => {
+    // console.log(countr)
     counter.innerHTML = 0
+
+    const updateCounter = () => {
+        const targetCount = +counter.getAttribute('data-target');
+        // console.log(typeof (targetCount));
+        const startingCount = Number(counter.innerHTML);
+        const inr = targetCount / 100;
+        if (startingCount < targetCount) {
+            counter.innerHTML = `${Math.round(startingCount + inr)}`;
+            setTimeout(updateCounter, 10);
+        } else {
+            counter.innerHTML = targetCount
+        }
+    }
+    updateCounter()
 })
